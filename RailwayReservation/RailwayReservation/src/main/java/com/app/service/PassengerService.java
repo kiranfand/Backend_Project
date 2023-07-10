17a7d02b3fb18c4ed89031486b470b8ce9335af4
Kiran Fand <kiranfand1912@gmail.com>
2023-07-10 10:46:15 +0530

    Add files via upload

---
+++ RailwayReservation/RailwayReservation/src/main/java/com/app/service/PassengerService.java
@@ -0,0 +1,12 @@
+package com.app.service;
+
+import java.util.List;
+
+import com.app.dto.PassengerDto;
+import com.app.entitites.Passaenger;
+
+public interface PassengerService {
+	List<Passaenger> getAllPassenger();
+      String addPassenger(PassengerDto pt);
+      String deletePAssenger(Long id);
+}
